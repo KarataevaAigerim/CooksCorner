7f7b4f83f3167176310fd3c40fa0b6926d6de3d3
aigerimkarataeva <ai.gerim@hotmail.com>
2024-07-28 20:16:39 +0600

    added api for fetching recipes

--- src/api/recipes.js
+++ src/api/recipes.js
@@ -4,3 +4,7 @@ const API_URL = 'https://cookscorner-production-9502.up.railway.app/api/v1';
 
 //  recipe API functions
 export const fetchRecipes = () => axios.get(`${API_URL}/recipes`);
+
+// Fetch recipes by category
+export const fetchRecipesByCategory = (category) => 
+    axios.get(`${API_URL}/recipes/by-category/${category.toUpperCase()}`);
--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,7 +1,7 @@
-// import React from 'react';
+// import React, { useState } from 'react';
 // import { useSelector } from 'react-redux';
-// import styles from './HomePage.module.scss';
 // import { Link } from 'react-router-dom';
+// import styles from './HomePage.module.scss';
 
 // // Import SVGs as components
 // import { ReactComponent as HomeIcon } from '../assets/svg/home.svg';
@@ -13,100 +13,82 @@
 
 // const HomePage = () => {
 //     const user = useSelector((state) => state.auth.user);
+//     const [activeTab, setActiveTab] = useState('Breakfast');
 
-//     function openCategory(evt, category) {
-//       // Declare all variables
-//       var i, tabcontent, tablinks;
-    
-//       // Get all elements with class="tabcontent" and hide them
-//       tabcontent = document.getElementsByClassName("tabcontent");
-//       for (i = 0; i < tabcontent.length; i++) {
-//         tabcontent[i].style.display = "none";
-//       }
-    
-//       // Get all elements with class="tablinks" and remove the class "active"
-//       tablinks = document.getElementsByClassName("tablinks");
-//       for (i = 0; i < tablinks.length; i++) {
-//         tablinks[i].className = tablinks[i].className.replace(" active", "");
-//       }
-    
-//       // Show the current tab, and add an "active" class to the button that opened the tab
-//       document.getElementById(category).style.display = "block";
-//       evt.currentTarget.className += " active";
+//     const openCategory = (category) => {
+//         setActiveTab(category);
 //     }
-  
+
 //     return (
-//       <div className={styles.container}>
 //         <div className={styles.container}>
-//           <nav>
-//             <div className={styles.navbar}>
-//               <div className={styles.logo}>
-//                 <SkilletIcon alt="Logo" className={styles.logoIcon}/>
-//                 <h3>CooksCorner</h3>
-//               </div>
-//               <div>
-//                 <LineIcon className={styles.line} />
-//               </div>
-//               <ul>
-//                 <li className={styles.navItem}>
-//                   <Link to="/home">
-//                     <HomeIcon className={styles.navIcon}/>
-//                   </Link>
-//                 </li>
-//                 <li className={styles.navItem}>
-//                   <Link to="/search">
-//                     <SearchIcon className={styles.navIcon} />
-//                   </Link>
-//                 </li>
-//                 <li className={styles.navItem}>
-//                   <Link to="/profile">
-//                     <ProfileIcon  className={styles.navIcon}/>
-//                   </Link>
-//                 </li>
-//                 <li className={styles.logout}>
-//                   <Link to="/logout">
-//                     <LogoutIcon  className={styles.navIcon}/>
-//                   </Link>
-//                 </li>
-//               </ul>
+//             <nav>
+//                 <div className={styles.navbar}>
+//                     <div className={styles.logo}>
+//                         <SkilletIcon alt="Logo" className={styles.logoIcon}/>
+//                         <h3>CooksCorner</h3>
+//                     </div>
+//                     <div>
+//                         <LineIcon className={styles.line} />
+//                     </div>
+//                     <ul>
+//                         <li className={styles.navItem}>
+//                             <Link to="/home">
+//                                 <HomeIcon className={styles.navIcon}/>
+//                             </Link>
+//                         </li>
+//                         <li className={styles.navItem}>
+//                             <Link to="/search">
+//                                 <SearchIcon className={styles.navIcon} />
+//                             </Link>
+//                         </li>
+//                         <li className={styles.navItem}>
+//                             <Link to="/profile">
+//                                 <ProfileIcon className={styles.navIcon}/>
+//                             </Link>
+//                         </li>
+//                         <li className={styles.logout}>
+//                             <Link to="/logout">
+//                                 <LogoutIcon className={styles.navIcon}/>
+//                             </Link>
+//                         </li>
+//                     </ul>
+//                 </div>
+//             </nav>
+//             <div className={styles.homepage}>
+//                 <p>Hello, Aigerim!</p>
+                // <div className={styles.title}>
+                //     <p className={styles.categoryText}>Category</p>
+                // </div>
+                // <div className={styles.content}>
+                //   <div className={styles.tab}>
+                //       {['Breakfast', 'Lunch', 'Dinner'].map((tab) => (
+                //           <button
+                //               key={tab}
+                //               className={`${styles.tablinks} ${activeTab === tab ? styles.active : ''}`}
+                //               onClick={() => openCategory(tab)}
+                //           >
+                //               {tab}
+                //           </button>
+                //       ))}
+                //   </div>
+                //   <div className={styles.tabcontent}>
+                //       {activeTab === 'Breakfast' && <div className={styles.recipeGrid}></div>}
+                //       {activeTab === 'Lunch' && <div className={styles.recipeGrid}></div>}
+                //       {activeTab === 'Dinner' && <div className={styles.recipeGrid}></div>}
+                //   </div>
+                // </div>
 //             </div>
-//           </nav>
-//           <div className={styles.homepage}>
-//             <p>Hello, Aigerim!</p>
-//             <div className={styles.recipeGrid}>
-//               <p className={styles.categoryText}>Category</p>
-//               {/* Tab links */}
-//               <div class="tab">
-//                 <button class="tablinks" onclick="openCategory(event, 'Breakfast')">Beakfast</button>
-//                 <button class="tablinks" onclick="openCategory(event, 'Lunch')">Lunch</button>
-//                 <button class="tablinks" onclick="openCategory(event, 'Dinner')">Dinner</button>
-//               </div>
-//                 {/* Tab content */}
-//               <div id="Breakfast" class="tabcontent">
-//                 <div className={styles.recipeGrid}></div>
-//               </div>
-
-//               <div id="Lunch" class="tabcontent">
-//                 <div className={styles.recipeGrid}></div>
-//               </div>
-
-//               <div id="Dinner" class="tabcontent">
-//                 <div className={styles.recipeGrid}></div>
-//               </div>
-//             </div>
-//           </div>
-          
 //         </div>
-//       </div>
 //     );
-//   };
-  
-//   export default HomePage;
+// };
 
-import React, { useState } from 'react';
+// export default HomePage;
+
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import styles from './HomePage.module.scss';
+import { fetchRecipesByCategory } from '../../api/recipes';  // Adjust the path as needed
 
 // Import SVGs as components
 import { ReactComponent as HomeIcon } from '../assets/svg/home.svg';
@@ -119,68 +101,75 @@ import { ReactComponent as LineIcon } from '../assets/svg/line.svg';
 const HomePage = () => {
     const user = useSelector((state) => state.auth.user);
     const [activeTab, setActiveTab] = useState('Breakfast');
+    const [recipes, setRecipes] = useState([]);
 
-    const openCategory = (category) => {
-        setActiveTab(category);
-    }
+    useEffect(() => {
+        fetchRecipesByCategory(activeTab)
+            .then(response => setRecipes(response.data))
+            .catch(error => console.error('Failed to fetch recipes', error));
+    }, [activeTab]);
 
     return (
         <div className={styles.container}>
             <nav>
-                <div className={styles.navbar}>
-                    <div className={styles.logo}>
-                        <SkilletIcon alt="Logo" className={styles.logoIcon}/>
-                        <h3>CooksCorner</h3>
-                    </div>
-                    <div>
-                        <LineIcon className={styles.line} />
-                    </div>
-                    <ul>
-                        <li className={styles.navItem}>
-                            <Link to="/home">
-                                <HomeIcon className={styles.navIcon}/>
-                            </Link>
-                        </li>
-                        <li className={styles.navItem}>
-                            <Link to="/search">
-                                <SearchIcon className={styles.navIcon} />
-                            </Link>
-                        </li>
-                        <li className={styles.navItem}>
-                            <Link to="/profile">
-                                <ProfileIcon className={styles.navIcon}/>
-                            </Link>
-                        </li>
-                        <li className={styles.logout}>
-                            <Link to="/logout">
-                                <LogoutIcon className={styles.navIcon}/>
-                            </Link>
-                        </li>
-                    </ul>
+              <div className={styles.navbar}>
+                <div className={styles.logo}>
+                    <SkilletIcon alt="Logo" className={styles.logoIcon}/>
+                    <h3>CooksCorner</h3>
                 </div>
+                <div>
+                    <LineIcon className={styles.line} />
+                </div>
+                <ul>
+                    <li className={styles.navItem}>
+                        <Link to="/home">
+                            <HomeIcon className={styles.navIcon}/>
+                        </Link>
+                    </li>
+                    <li className={styles.navItem}>
+                        <Link to="/search">
+                            <SearchIcon className={styles.navIcon} />
+                        </Link>
+                    </li>
+                    <li className={styles.navItem}>
+                        <Link to="/profile">
+                            <ProfileIcon className={styles.navIcon}/>
+                        </Link>
+                    </li>
+                    <li className={styles.logout}>
+                        <Link to="/logout">
+                            <LogoutIcon className={styles.navIcon}/>
+                        </Link>
+                    </li>
+                  </ul>
+              </div>
             </nav>
             <div className={styles.homepage}>
-                <p>Hello, Aigerim!</p>
+                <p>Hello, !</p>
                 <div className={styles.title}>
                     <p className={styles.categoryText}>Category</p>
-                </div>
-                <div className={styles.content}>
-                  <div className={styles.tab}>
-                      {['Breakfast', 'Lunch', 'Dinner'].map((tab) => (
-                          <button
-                              key={tab}
-                              className={`${styles.tablinks} ${activeTab === tab ? styles.active : ''}`}
-                              onClick={() => openCategory(tab)}
-                          >
-                              {tab}
-                          </button>
-                      ))}
-                  </div>
-                  <div className={styles.tabcontent}>
-                      {activeTab === 'Breakfast' && <div className={styles.recipeGrid}></div>}
-                      {activeTab === 'Lunch' && <div className={styles.recipeGrid}></div>}
-                      {activeTab === 'Dinner' && <div className={styles.recipeGrid}></div>}
-                  </div>
+                    <div className={styles.content}>
+                      <div className={styles.tab}>
+                          {['Breakfast', 'Lunch', 'Dinner'].map((tab) => (
+                              <button
+                                  key={tab}
+                                  className={`${styles.tablinks} ${activeTab === tab ? styles.active : ''}`}
+                                  onClick={() => setActiveTab(tab)}
+                              >
+                                  {tab}
+                              </button>
+                          ))}
+                      </div>
+                      <div className={styles.tabcontent}>
+                          {recipes.map(recipe => (
+                              <div key={recipe.id} className={styles.recipeCard}>
+                                  <img src={recipe.image} alt={recipe.title} />
+                                  <h3>{recipe.title}</h3>
+                                  <p>{recipe.description}</p>
+                              </div>
+                          ))}
+                      </div>
+                    </div>
                 </div>
             </div>
         </div>
